92ec81da806a9b9e00c6e7b409bff575d4c155dd
Jordan <jtate1166@gmail.com>
2022-04-14 17:12:58 -0400

    Remove 'content' class, update css in 'main' html element

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function App() {
   return (
     <main>
-      <div className="content">hello world!</div>
+      <div>hello world!</div>
     </main>
   );
 }
