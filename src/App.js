a8f4773384dc442e2fb14963c39c38f7ecea179a
Jordan <jtate1166@gmail.com>
2022-04-16 11:48:55 -0400

    Initialize App state and render Die components dynamically

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Die from "./Die";
 
 export default function App() {
+  const [diceNumbers, setDiceNumbers] = useState(allNewDice());
+
   function allNewDice() {
     const randomNumbersArray = Array(10)
       .fill()
       .map(() => Math.ceil(Math.random() * 6));
-    console.log(randomNumbersArray);
+    return randomNumbersArray;
   }
-  allNewDice();
+
   return (
     <main>
       <div className="dice-container">
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={7} />
-        <Die value={8} />
-        <Die value={9} />
-        <Die value={10} />
+        {diceNumbers.map((number) => (
+          <Die value={number} />
+        ))}
       </div>
     </main>
   );
